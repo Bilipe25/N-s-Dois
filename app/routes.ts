1631d88e061c0b056dc9f978e51bf8758e35144d
Gabriel Felipe <gabriellion97@gmail.com>
2025-11-30 12:56:26 -0300

    erro de router api subretip

--- app/routes.ts
+++ app/routes.ts
@@ -27,4 +27,5 @@ export default [
 
     route("public/bridal-shower", "routes/public.bridal-shower.tsx"),
     route("public/wedding", "routes/public.wedding.tsx"),
+    route("api/subscribe", "routes/api.subscribe.tsx"),
 ] satisfies RouteConfig;
