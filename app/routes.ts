8f5d7ce9d0536b7cb1c520adef434691383b360c
Gabriel Felipe <gabriellion97@gmail.com>
2025-11-30 13:18:02 -0300

    test push

--- app/routes.ts
+++ app/routes.ts
@@ -28,4 +28,5 @@ export default [
     route("public/bridal-shower", "routes/public.bridal-shower.tsx"),
     route("public/wedding", "routes/public.wedding.tsx"),
     route("api/subscribe", "routes/api.subscribe.tsx"),
+    route("test-push", "routes/test-push.tsx"),
 ] satisfies RouteConfig;
